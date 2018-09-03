bb5032c0282e596c9247f5a9e8bf36070bf1499c
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2018-09-02 19:25:35 -0700

    More edits

--- alex.engelberg.lol/src/alex/core.cljs
+++ alex.engelberg.lol/src/alex/core.cljs
@@ -1,22 +1,28 @@
 (ns alex.core
-    (:require [reagent.core :as reagent :refer [atom]]))
+  (:require
+    [reagent.core :as reagent :refer [atom]]))
 
 (enable-console-print!)
 
-(println "This text is printed from src/alex/core.cljs. Go ahead and edit it and see reloading in action.")
+(println "Hi")
 
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce app-state
+  (atom {:title "Welcome to Alex's site"
+         :text "Cool"}))
 
 
 (defn hello-world []
   [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this and watch it change!"]])
+   [:h1 (:title @app-state)]
+   [:p (:text @app-state)]])
 
-(reagent/render-component [hello-world]
-                          (. js/document (getElementById "app")))
+
+(reagent/render-component
+  [hello-world]
+  (. js/document (getElementById "app")))
+
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
